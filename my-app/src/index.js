90c419d48ba97ea1b8905d06add1cd657d870ce5
LinoyHorvat <linoy.horvat@gmail.com>
2021-12-19 09:13:35 +0200

    first commit

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,22 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Game from './Game';
 
-// class Game extends React.Component {
-//   state = {
-//     curPlayer : 1
-//   }
-//   render() {
-//     return (
-//       <div>
-//       <button>NEW GAME</button>
-//       <button>ROLL DICE</button>
-//       <button>HOLD</button>
-//       <button>Final Score = 100 </button>
-//       </div>
-//     )
-//   }
-// }
-
 ReactDOM.render(
     <Game />,
   document.getElementById('root')
